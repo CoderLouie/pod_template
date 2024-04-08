6912cb04109088032ffb826ebc562945d3de526e
liyang <18616562401@163.com>
2024-04-08 11:28:11 +0800

    1. 调整swift模版

--- templates/swift/Example/Example for PROJECT/AppDelegate.swift
+++ templates/swift/Example/Example for PROJECT/AppDelegate.swift
@@ -12,12 +12,20 @@ class AppDelegate: UIResponder {
     
     var window: UIWindow?
     
+    static var shard: AppDelegate? {
+        UIApplication.shared.delegate as? AppDelegate
+    }
 }
 
 extension AppDelegate: UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.makeKeyAndVisible()
+        self.window = window
         
         return true
     }
--- templates/swift/Example/Example for PROJECT/ViewController.swift
+++ templates/swift/Example/Example for PROJECT/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import SwifterKnife
+
+fileprivate class TestCaseCell: UITableViewCell, Reusable {
+}
+
+fileprivate enum TestCase: String, CaseIterable {
+    case one = "one"
+    
+    func perform(from vc: ViewController) {
+        switch self {
+        case .one:
+            print("one")
+        }
+    }
+    
+}
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        tableView = UITableView().then {
+            $0.backgroundColor = .clear
+            $0.tableFooterView = UIView()
+            $0.contentInsetAdjustmentBehavior = .never
+            $0.separatorStyle = .none
+            $0.delegate = self
+            $0.dataSource = self
+            $0.rowHeight = 45
+            $0.register(cellType: TestCaseCell.self)
+            view.addSubview($0)
+            $0.contentInset = UIEdgeInsets(top: Screen.navbarH, left: 0, bottom: Screen.safeAreaB, right: 0)
+            $0.frame = view.bounds
+        }
     }
+    private unowned var tableView: UITableView!
+    private lazy var items: [TestCase] = TestCase.allCases
+}
 
+// MARK: - Delegate
+extension ViewController: UITableViewDataSource {
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       items.count
+   }
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell: TestCaseCell = tableView.dequeueReusableCell(for: indexPath)
+       let index = indexPath.row
+       let text = String(format: "%02d. ", index) + items[index].rawValue
+       cell.textLabel?.text = text
+       return cell
+   }
+}
 
+extension ViewController: UITableViewDelegate {
+   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       tableView.deselectRow(at: indexPath, animated: true)
+       items[indexPath.row].perform(from: self)
+   }
 }
 
